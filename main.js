4bc431b1f5437c24e1a8fea8b08e8a1fce203945
Shivangi <129336967+Chaurasiya7@users.noreply.github.com>
2023-07-20 22:52:55 +0530

    commit

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // Close the cart modal when the close button is clicked
+  $('#cartModal .close').click(function() {
+    $('#cartModal').modal('hide');
+  });
+});
